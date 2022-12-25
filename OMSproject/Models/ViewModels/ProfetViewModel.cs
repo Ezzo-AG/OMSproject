6abd1929478b58ea32a890b3bc996879c27d6f62
azmialwerfalli <drama.com.aa@gmail.com>
2022-12-25 18:13:06 +0200

    1.0024v Update

--- OMSproject/Models/ViewModels/ProfetViewModel.cs
+++ OMSproject/Models/ViewModels/ProfetViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace OMSproject.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace OMSproject.Models.ViewModels
 {
     public class ProfetViewModel
     {
         public int? OrderId { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime OrderDate { get; set; }
         public float OrderCoast { get; set; }
         public float OrderSellPrice { get; set; }
