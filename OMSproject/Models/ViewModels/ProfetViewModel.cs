2db4f71ba307bf1c51b34b59754a520677ad2e99
ezzo-ag <ezzogr26@gmail.com>
2022-12-25 17:22:52 +0200

    1.0011v profetFunction

---
+++ OMSproject/Models/ViewModels/ProfetViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OMSproject.Models.ViewModels
+{
+    public class ProfetViewModel
+    {
+        public int? OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public float OrderCoast { get; set; }
+        public float OrderSellPrice { get; set; }
+        public float Profet { get; set; }
+    }
+}
