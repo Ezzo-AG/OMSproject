dba8d4ce38923351156e551612735690737b2538
azmialwerfalli <drama.com.aa@gmail.com>
2023-01-30 10:11:13 +0200

    New UI Changes

---
+++ OMSproject/wwwroot/styles/js/app.js
@@ -0,0 +1,37 @@
+// let btn = document.querySelector("#btn");
+// let sidebar = document.querySelector(".sidebar");
+// let searchBtn = document.querySelector(".bx-search");
+
+// btn.onclick = function() {
+//     sidebar.classList.toggle("active");
+// }
+// searchBtn.onclick = function() {
+//     sidebar.classList.toggle("active");
+// }
+
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light mode";
+    } else {
+        modeText.innerText = "Dark mode";
+
+    }
+});
